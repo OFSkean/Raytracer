f2d75eef7eba63ee39736262c32b4a0df0bf628c
ofskean727 <ofskean727@gmail.com>
2019-09-02 11:55:35 -0400

    Made light revolve around sphere

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cmath>
 #include <fstream>
@@ -16,15 +18,19 @@ struct Vector {
     }
 };
 
-
-double dot(Vector a, Vector b) {
-    return a.x*b.x + a.y*b.y + a.z*b.z;
-}
-
 struct Color {
-    short r, b, g;
-    Color() {r=g=b=0;}
-    Color(short R, short B, short G) {r=R; b=B; g=G;}
+    double r, b, g;
+    Color() {r=b=g=0;}
+    Color (double R, double B, double G) {r=R; b=B; g=G;}
+    Color operator * (double d) {
+        //saturate so that values don't over/underflow
+        if (d > 0) {
+            return { std::min(d*r, 255.0), std::min(d*b, 255.0), std::min(d*g, 255.0) };
+        }
+        else {
+           return {};
+        }
+    }
 };
 
 struct Ray {
@@ -33,6 +39,10 @@ struct Ray {
     Ray(Vector a, Vector b) {origin=a; direction=b;}
 };
 
+double dot(Vector a, Vector b) {
+    return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
 struct Sphere {
     Vector center;
     double radius;
@@ -40,14 +50,14 @@ struct Sphere {
     Sphere(Vector c, double r) {center=c; radius=r;}
 
     Vector getNormal(Vector v) {
-        return (v - center) / radius;
+        return (center - v) / radius;
     }
-    bool intersects(Ray ray, double &t) {
+    bool intersects(Ray &ray, double &t) {
         Vector oc = ray.origin - center;
 
         double disc_a = 1;    // a = dot(ray.dest, ray.dest) = 1
         double disc_b = 2*dot(oc, ray.direction);
-        double disc_c = 1 - radius*radius;
+        double disc_c = dot(oc, oc) - radius*radius;
 
         double discriminant = disc_b*disc_b - 4*disc_a*disc_c;
         if (discriminant < 0) {
@@ -55,55 +65,59 @@ struct Sphere {
         }
         else {
             discriminant = sqrt(discriminant);
-            double t0 = -disc_b + discriminant;
-            double t1 = -disc_b - discriminant;
+            double t0 = (-disc_b - discriminant)/(2*disc_a);
+            double t1 = (-disc_b + discriminant)/(2*disc_a);
             t = (t0 < t1) ? t0 : t1;
             return true;
         }
     }
 };
 
-void saturate(Vector& color) {
-    color.x = (color.x > 255) ? 255 : (color.x < 0) ? 0 : color.x;
-    color.y = (color.y > 255) ? 255 : (color.y < 0) ? 0 : color.y;
-    color.z = (color.z > 255) ? 255 : (color.z < 0) ? 0 : color.z;
-}
 int main() {
     const int H = 500;
     const int W = 500;
+    const double timeIncrement = M_PI/100;
 
-    Vector white(255, 255, 255);
-    Vector black(0, 0, 0);
-    Vector red(255, 0, 0);
+    Color white(255, 255, 255);
+    Color black;
+    Color pixel_color;
 
-    Sphere sphere(Vector(W*0.5, H*0.5, 50), 50);
-    Sphere light(Vector(0, 0, 5), 1);
-    Vector camera(Vector(0, 0, 1));
+    Sphere sphere(Vector(W*0.5, H*0.5, 250), 50);
 
-    std::ofstream render("render.ppm");
-    render << "P3\n" << W << ' ' << H << ' ' << "255\n";
+    double t = 20000;
+    double time = 0;
+    int imageCounter = 0;
+    double tau = 2*M_PI;
+    int imageCount = (int)((2*tau)/timeIncrement);
+    while (time < tau) {
+        Sphere light(Vector(W*0.5, 250+250*std::sin(time), 250+250*std::sin(time-M_PI_2)), 1);
+        time += timeIncrement;
 
-    double t;
-    Vector pixel_color(black);
+        //std::cout << light.center.x << " " << light.center.y << " " << light.center.z << std::endl;
 
-    for (int y = 0; y < H; ++y) {
-        for (int x = 0; x < W; ++x) {
-            pixel_color = black;
+        std::cout << "Rendering: " << imageCounter << " / " << imageCount << std::endl;
+        std::ofstream render("images/render" + std::to_string(imageCounter++) + ".ppm");
+        render << "P3\n" << W << ' ' << H << ' ' << "255\n";
 
-            Ray ray(Vector(x, y, 0), camera);
+        for (int y = 0; y < H; ++y) {
+            for (int x = 0; x < W; ++x) {
+                pixel_color = black;
 
-            if (sphere.intersects(ray, t)) {
-                Vector extended = ray.origin + ray.direction*t;
-                Vector L = light.center - extended;
-                Vector N = sphere.getNormal(extended);
-                double dt = dot(L.normalize(), N.normalize());
+                Ray ray(Vector(x, y, 0), Vector(0, 0, 1));
 
-                pixel_color = (red + white*dt) * 0.5;
-                saturate(pixel_color);
+                if (sphere.intersects(ray, t)) {
+                    Vector intersection_point = ray.origin + ray.direction * t;
+                    Vector L = intersection_point - light.center;
+                    Vector N = sphere.getNormal(intersection_point);
+                    double dt = dot(L.normalize(), N.normalize());
+
+                    pixel_color = white * dt;
+                }
+                render << (int) pixel_color.r << std::endl
+                       << (int) pixel_color.b << std::endl
+                       << (int) pixel_color.g << std::endl;
             }
-            render << (int)pixel_color.x << ' '
-                   << (int)pixel_color.y << ' '
-                   << (int)pixel_color.z << '\n';
         }
+        render.close();
     }
 }
